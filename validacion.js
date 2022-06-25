1cb9c68beef4a3ec5d6edab2a6e80c9a62a9390f
Marcelo Simon <marcelo.simon@gmail.com>
2022-06-25 03:42:04 -0300

    Página terminada

--- validacion.js
+++ validacion.js
@@ -1,1 +1,20 @@
-//Haz tú validación en javascript acá
+function chequearFormulario(){
+    let nombre = document.querySelector(".formcontacto__input__nombre").value;
+    let email = document.querySelector(".formcontacto__input__email").value;
+    let asunto = document.querySelector(".formcontacto__input")
+    let mensaje = document.querySelector(".formcontacto__textarea").value;
+    let expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if ((email == "") || (nombre == "") || (mensaje == "") || (asunto == "")) {  //COMPRUEBA CAMPOS VACIOS
+        alert("Los campos no pueden quedar vacios");
+        formularioCompleto = false;
+        return;
+    }
+    else if ( !expr.test(email) ){                              //COMPRUEBA MAIL
+        alert('Error: La dirección de correo "' + email + '" es incorrecta.');
+        formularioCompleto = false;
+        return;
+    }
+    else{
+        formularioCompleto = true;
+    };
+}
